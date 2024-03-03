e6b8e2c90334923d434f2a2be8b06a47500d324e
CommandoNK18 <152832842+CommandoNK18@users.noreply.github.com>
2024-03-03 22:24:56 +0530

    Add files via upload

--- navbar.js
+++
@@ -1,9 +0,0 @@
-function gfgMenu() { 
-    const GFG = document.querySelector('.links'); 
-    if (GFG.classList.contains('d-none')) { 
-        GFG.classList.remove('d-none'); 
-    } 
-    else { 
-        GFG.classList.add('d-none'); 
-    } 
-} 
